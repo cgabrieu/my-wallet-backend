805af26b6192f4634e2316528a56c13dd0c56d26
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-27 11:47:02 -0300

    refactor(auth): create findByEmail & createUser repositories

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,42 +1,20 @@
-import { v4 as uuid } from 'uuid';
 import bcrypt from 'bcrypt';
 import connection from '../database/database.js';
 import * as userSchemas from '../schemas/userSchemas.js';
 
 export async function signIn(req, res) {
   try {
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
     if (userSchemas.signIn.validate(req.body).error) {
       return res.status(400).send('Dados inválidos.');
     }
 
-    const hasUser = await connection.query(
-      `SELECT * FROM users
-      WHERE email = $1;`,
-      [email],
-    );
+    return res.status(200).send({
+      token: 'teste',
+    });
 
-    const user = hasUser.rows[0];
-
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = uuid();
-
-      await connection.query(
-        `INSERT INTO sessions
-        ("userId", token)
-        VALUES ($1, $2);`,
-        [user.id, token],
-      );
-
-      return res.status(200).send({
-        userId: user.id,
-        name: user.name,
-        email,
-        token,
-      });
-    }
-    return res.status(401).send('E-mail ou senha inválidos.');
+    // return res.status(401).send('E-mail ou senha inválidos.');
   } catch (error) {
     return res.status(500);
   }
---
+++ src/repositories/userRepository.js
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+import connection from '../database/database';
+
+export async function findByEmail(email) {
+  const user = await connection.query(
+    'SELECT * FROM "users" WHERE "email" = $1',
+    [email],
+  );
+  return user.rows[0];
+}
+
+export async function createUser(name, email, password) {
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  await connection.query(
+    'INSERT INTO "users" ("name", "email", "password") VALUES ($1, $2, $3)',
+    [name, email, hashedPassword],
+  );
+}
