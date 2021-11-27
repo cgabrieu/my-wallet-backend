fb250ea3806ad3e4d55c571b0b362888f3c66dbb
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-27 11:14:34 -0300

    feat(auth): create jwt middleware

---
+++ src/middlewares/authenticationJWT.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+export default async function authenticationJWT(req, res, next) {
+  const authorization = req.header('Authorization');
+  const token = authorization?.replace('Bearer ', '');
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(401);
+    req.userId = decoded.userId;
+    return next();
+  });
+}
